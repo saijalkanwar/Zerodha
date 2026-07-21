6319678076e589b65558fd315325d363ddba6139
Saijal Kanwar <saijalkanwar71@gmail.com>
2026-07-21 23:22:33 +0530

    created footer,navbar and openaccount

--- frontend/src/landing_page/Footer.js
+++ frontend/src/landing_page/Footer.js
@@ -1,9 +1,61 @@
 import React from "react";
-function Footer(){
-    return(
-        <div>   
-            Footer
-        </div>              
-    );
+
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: "rgb(250, 250, 250)" }}>
+      <div className="container border-top mt-5">
+        <div className="row mt-5">
+          <div className="col">
+            <img src="media/logo.svg" style={{ width: "50%" }} />
+            <p>
+              &copy; 2010 - 2024, Not Zerodha Broking Ltd. All rights reserved.
+            </p>
+          </div>
+          <div className="col">
+            <p>Company</p>
+            <a href="">About</a>
+            <br />
+            <a href="">Products</a>
+            <br />
+            <a href="">Pricing</a>
+            <br />
+            <a href="">Referral programme</a>
+            <br />
+            <a href="">Careers</a>
+            <br />
+            <a href="">Zerodha.tech</a>
+            <br />
+            <a href="">Press & media</a>
+            <br />
+            <a href="">Zerodha cares (CSR)</a>
+            <br />
+          </div>
+          <div className="col">
+            <p>Support</p>
+            <a href="">Contact</a>
+            <br />
+            <a href="">Support portal</a>
+            <br />
+            <a href="">Z-Connect blog</a>
+            <br />
+            <a href="">List of charges</a>
+            <br />
+            <a href="">Downloads & resources</a>
+            <br />
+          </div>
+          <div className="col">
+            <p>Account</p>
+            <a href="">Open an account</a>
+            <br />
+            <a href="">Fund transfer</a>
+            <br />
+            <a href="">60 day challenge</a>
+            <br />
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
+
 export default Footer;
--- frontend/src/landing_page/OpenAccount.js
+++ frontend/src/landing_page/OpenAccount.js
@@ -1,8 +1,15 @@
 import React from 'react';
 function OpenAccount(){
     return(
-        <div>
-            OpenAccount
+        <div className='container p-5 mb-5'>
+            <div className='row text-center'>
+        <img src='media/homehero.png' alt="Hero" className='mb-5' />
+        <h1 className="mt-5">Open a Zerodha Account</h1>
+        <p>
+          Modern platforms and apps, 0 investment advisory, and a transparent pricing model. Open an account in 10 minutes.
+        </p>
+        <button className='p-2 btn btn-primary fs-5' style={{width:"30%",margin:"0 auto"}}> Sign Up Now </button>
+        </div>
         </div>
     );
 }
