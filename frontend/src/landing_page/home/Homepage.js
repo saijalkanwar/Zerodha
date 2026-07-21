8ea70f331a2af319285f0c70f54e312ae9c001c9
Saijal Kanwar <saijalkanwar71@gmail.com>
2026-07-21 21:56:49 +0530

    Created all the home page components

---
+++ frontend/src/landing_page/home/Homepage.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Hero from "./Hero";
+import Stats from "./Stats";
+import Pricing from "./Pricing";
+import Awards from "./Awards";
+import Education from "./Education";
+import OpenAccount from "../OpenAccount";
+import Footer from "../Footer";
+import Navbar from "../Navbar";
+
+function Homepage(){
+    return(
+        <div>
+            <Navbar/>
+            <Hero/>
+            <Awards/>
+            <Stats/>    
+            <Pricing/>
+            <Education/>
+            <OpenAccount/>
+            <Footer/>
+                
+        </div>
+
+    );
+}   
+export default Homepage;
